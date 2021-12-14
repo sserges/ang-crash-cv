79ede7db315586f5a39e1a38f416bbdc5fed1695
sserges <agounkeserge@gmail.com>
2021-12-13 22:35:25 -0400

    Two Way Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +7,9 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TestComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -18,44 +18,9 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class TestComponent implements OnInit {
-  public name = 'Bro';
-  public siteUrl = window.location.href;
-  public myId = 'testId';
-  public isDisabled = true;
-  public successClass = 'text-success';
-
-  public hasError = false;
-  public isSpecial = true;
-
-  public highlightColor = 'orange';
-
-  public greeting = '';
-
-  public messageClasses = {
-    'text-success': !this.hasError,
-    'text-danger': this.hasError,
-    'text-special': this.isSpecial,
-  };
-
-  public titleStyles = {
-    color: 'blue',
-    fontStyle: 'italic',
-  };
+  public name = '';
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  greetUser() {
-    return 'Hello ' + this.name;
-  }
-
-  onClick(event: Event) {
-    console.log(event);
-    this.greeting = event.type;
-  }
-
-  logMessage(value: any) {
-    console.log(value);
-  }
 }
